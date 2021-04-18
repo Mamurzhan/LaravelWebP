ad6238f05809b629fcece77cae5ef55990762b12
Mamurzhan <190103286@stu.sdu.edu.kz>
2021-04-18 13:09:09 +0300

    first commit

---
+++ app/Http/Controllers/Sdu12Controller.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Sdu12;
+class Sdu12Controller extends Controller
+{
+    public function addStory(){
+        return view('add-story');
+    }
+    public function storeStory(Request $request){
+        $name = $request->name;
+        $email = $request->email;
+        $topic = $request->topic;
+        $story = $request->story;
+        $image = $request->file;
+        $imageName = time().'.'.$image->extension();
+        $image->move(public_path('images'),$imageName);
+
+        $sdu12 = new Sdu12();
+        $sdu12->name = $name;
+        $sdu12->email = $email;
+        $sdu12->topic = $topic;
+        $sdu12->story = $story;
+        $sdu12->image = $imageName;
+        $sdu12->save();
+        return back()->with('story_added' , 'story record has been inserted');
+
+    }
+public function users(){
+
+    $sduies = Sdu12::all();
+
+    return view('all-users',compact('sduies'));
+}
+public function editUser($id){
+    $sdu12 =Sdu12::find($id);
+    return view('edit-user',compact('sdu12'));
+}
+public function updateUser(Request $request){
+    $name = $request->name;
+    $email = $request->email;
+    $topic = $request->topic;
+    $story = $request->story;
+    $image = $request->file;
+    $imageName = time().'.'.$image->extension();
+    $image->move(public_path('images'),$imageName);
+
+    $sdu12 = Sdu12::find($request->id);
+        $sdu12->name = $name;
+        $sdu12->email = $email;
+        $sdu12->topic = $topic;
+        $sdu12->story = $story;
+        $sdu12->image = $imageName;
+        $sdu12->save();
+        return back()->with('user_update' , 'Story updated successfully!');
+}
+public function deleteUser($id){
+    $sdu12 = Sdu12::find($id);
+    unlink(public_path('images').'/'.$sdu12->image);
+    $sdu12->delete();
+    return back()->with('user_deleted','User deleted successfully!');
+}
+function index(){
+return view('send_email');
+}
+function send(Request $request){
+    $this->validate($request,[
+        'name'     =>    'required',
+        'email'    =>    'required|email',
+        'message'  =>    'required'
+    ]);
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Sdu12Controller;
+use App\Http\Controllers\EmailController;
+/*
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/add-story',[Sdu12Controller::class,'addStory']);
+Route::post('/add-story',[Sdu12Controller::class,'storeStory'])->name('story.store');
+Route::get('/all-user',[Sdu12Controller::class,'users']);
+Route::get('/edit-user/{id}',[Sdu12Controller::class,'editUser']);
+Route::post('/update-user',[Sdu12Controller::class,'updateUser'])->name('user.update');
+Route::get('/delete-user/{id}',[Sdu12Controller::class,'deleteUser']);
+Route::get('/sendemail',[EmailController::class,'index']);
+Route::post('/sendemail/send',[EmailController::class,'send']);
